4c1bcbd1585f38978deccffa84445d71b815bcc3
Muhammad Ilham <hi@muhammadilham.xyz>
2025-02-02 18:51:01 +0700

    feat: adjust landing page positioning

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,25 +1,17 @@
 <template>
   <div id="cover">
+    <img id="logo" src="@/assets/Logo.png" alt="Wedding Logo">
     <div id="header">
-      <!-- <span id="title">- The Wedding of -</span> -->
       <span id="date">... today, tomorrow, and forever</span>
     </div>
-    <br />
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    scrollToSecond() {
-      this.$emit("openInvitationClicked");
-    }
-  }
-};
+export default {};
 </script>
 
 <style scoped>
-/* General Reset */
 html, body {
   margin: 0;
   padding: 0;
@@ -27,47 +19,25 @@ html, body {
   overflow-x: hidden;
 }
 
-/* Header with Logo */
-#header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1;
-  background-image: url('@/assets/Logo.png'); /* Set logo as background */
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center;
-  width: 100vw;
-  height: 40vh; /* Adjust height to fit the logo */
-  position: relative;
+#logo {
+  width: 300px;
+  height: auto;
+  max-width: 60%;
+  margin-top: 20px;
+  margin-bottom: 25px;
 }
 
-/* Wedding Title */
-#title {
-  font-family: "Jura", serif;
-  color: white;
-  font-weight: 500;
-  font-size: 2em;
-  position: absolute;
-  top: -10px; /* Move the title above the logo */
-  margin: 0;
-}
-
-/* Date Under Title */
 #date {
   font-family: "Jura", serif;
   color: white;
   font-size: 1.5em;
-  position: absolute;
-  bottom: -10px; /* Move the date below the logo */
-  margin: 0;
+  text-align: center;
 }
 
-/* Fullscreen Cover Section */
 #cover {
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   height: 100vh;
   width: 100vw;
@@ -76,9 +46,9 @@ html, body {
   background-repeat: no-repeat;
   position: relative;
   background-color: black;
+  padding-top: 20vh;
 }
 
-/* Dark Overlay Effect */
 #cover::before {
   content: "";
   position: absolute;
@@ -90,24 +60,15 @@ html, body {
   z-index: 0;
 }
 
-/* Centering the Button */
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1;
-}
-
-/* Responsive Adjustments */
 @media (max-width: 768px) {
-  #title {
-    font-size: 1em;
-    top: -10px;
+  #logo {
+    max-width: 70%;
+    margin-top: 10px;
+    margin-bottom: 20px;
   }
 
   #date {
-    font-size: 1em;
-    bottom: -10px;
+    font-size: 1.2em;
   }
 }
 </style>
